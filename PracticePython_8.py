792be682af7a03664fd680c30b89a819c1e33d19
Adam <adambatchelor2@gmail.com>
2019-12-20 21:24:08 +0000

    P8

--- PracticePython_8.py
+++ PracticePython_8.py
@@ -6,3 +6,24 @@
 #Rock beats scissors
 #Scissors beats paper
 #Paper beats rock
+
+import random
+
+option = ['r','p','s']
+
+p1_input = input("P1 choose (r)ock, (p)aper or (s)cissors?")
+
+p2_input = option[random.randint(0, 2)]
+
+print ("\nComputer chooses "+ p2_input +"\n")
+
+if p1_input == 'r' and p2_input != 'r' and p2_input != 'p':
+	print ("P1 wins!")
+elif p1_input == 'p' and p2_input != 'p' and p2_input != 's':
+	print ("P1 wins!")
+elif p1_input == 's' and p2_input != 's' and p2_input != 'r':
+	print ("P1 wins!")
+elif p1_input == p2_input:
+	print("Draw")
+else: 
+	print("Computer wins!")
