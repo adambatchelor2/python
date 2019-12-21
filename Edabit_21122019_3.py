a302245e62c7ece2c65c7455587a8e9204b53c5e
Adam <adambatchelor2@gmail.com>
2019-12-21 22:40:07 +0000

    More

---
+++ Edabit_21122019_3.py
@@ -0,0 +1,10 @@
+#Write a function that takes a string, and returns a new string with any duplicate consecutive letters removed.
+#i.e. Streeeeeetch to Stretch
+
+#def removeDupe(inStr):
+#	for x in range (1,inStr.length)
+
+
+inStr = str("Streeeeeetch")
+
+print (inStr.len)
