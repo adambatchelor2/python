15e7c1a91155c1fe0be4d62a6913ed504725419a
Adam <36477769+adambatchelor2@users.noreply.github.com>
2020-06-23 19:22:44 +0100

    Update Edabit_220620_true_count.py

---
+++ Edabit_220620_true_count.py
@@ -0,0 +1,11 @@
+# Create a function which returns the number of true values there are in an array.
+# Return 0 if given an empty array.
+# All array items are of the type bool (true or false).
+# countTrue([true, false, false, true, false]) ➞ 2
+
+listIn = [true, false, false, true, false]
+
+print("A")
+
+# for x in listIn
+# print(len(listIn))
