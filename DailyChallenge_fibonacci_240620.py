a535ae75c827f6d6898e0ecda07bfdc8fe457a57
Adam <adambatchelor2@gmail.com>
2020-06-24 10:16:32 +0100

    New challenge

--- DailyChallenge_fibonacci_240620.py
+++ DailyChallenge_fibonacci_240620.py
@@ -2,12 +2,17 @@
 # Ask the user to enter the Nth term and have the program calculate the sequence
 # until it has printed that many terms.
 
-#0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 
-nth = 5
-fibonnaci = [0]
+def fib_input(nth_count):
+    fib_list = [0, 1]
 
-for x in range(1,nth):
-    #calc next value
-    print(x)
-    #append next value to list
+    for x in range(1, nth_count - 1):
+        # calc next value
+        new_val = fib_list[x] + fib_list[x - 1]
+        # print(new_val)
+        fib_list.append(new_val)
+        # append next value to list
+    return fib_list
+
+print(fib_input(9))
