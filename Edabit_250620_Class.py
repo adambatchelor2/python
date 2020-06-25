9c2a3366b68fb16f0f876063b78e2f22db1daa1b
Adam <36477769+adambatchelor2@users.noreply.github.com>
2020-06-25 21:22:18 +0100

    Fail

---
+++ Edabit_250620_Class.py
@@ -0,0 +1,11 @@
+# Given an int, figure out how many ones, threes and nines you could fit into the number.
+# You must create a class. You will make variables(self.ones, self.threes, self.nines) to do this.
+
+
+class test:
+    x = 5
+    y = 2
+
+p = test()
+
+print(p.y)
