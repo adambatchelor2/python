1cbf3f98a132d0fccf805a7491af6e2cdbec4ff4
Adam <adambatchelor2@gmail.com>
2020-06-30 10:04:12 +0100

    New challenge

--- DailyChallenge_hangman_290620.py
+++ DailyChallenge_hangman_290620.py
@@ -11,17 +11,19 @@ class hangman:
         self.win = False
         self.current_string = ['x' for x in range(len(self.hidden_string))]
 
-
+    #Function to replace characters from current string with guessed character
     def char_replace(self, inStr):
         char_loc = self.hidden_string.find(inStr)
         self.current_string[char_loc] = inStr
 
+    #If guess correct replace character else increase guess count
     def guess(self,inStr):
         if inStr in self.hidden_string:
             self.char_replace(inStr)
         else:
             self.guess_count += 1
 
+    #Check to see if game complete
     def win(self):
         if self.current_string == self.hidden_string:
             self.win == True
@@ -29,6 +31,7 @@ class hangman:
             self.win == False
 
 
+    #Main guess checking function
     def guess_check(self,inStr):
 
         self.guess(inStr)
@@ -38,13 +41,11 @@ class hangman:
         else:
             return (f"Try Again - {self.current_string}")
 
+
 game1 = hangman("ytest", 3)
 # game2 = hangman("loooong",3)
 
-#print(game1.current_string)
-# print(new_game.test())
-# print(second_game.test())
-#
+
 while game1.guess_count < game1.guess_limit and game1.win == False:
      game1.guess_check(input("Guess letter:"))
 
