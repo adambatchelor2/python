66927c3eaa4eb34d35a1d8b9aa0dad449e62eda5
Adam <adambatchelor2@gmail.com>
2020-09-04 16:48:50 +0100

    Update

---
+++ pdf_scrape.py
@@ -0,0 +1,15 @@
+import camelot
+import pandas as pd
+# read in pdf
+tables = camelot.read_pdf("ACI.pdf",  flavor='stream', pages='8,9,10,11,12,13')
+# Loop through each table (table per page)
+test_df = pd.DataFrame() # empty df
+for table in tables:
+    test_df = test_df.append(table.df) # add to dataframe
+# remove headers that are not needed.
+test_df = test_df[test_df[1] != "CITY"]
+test_df = test_df[test_df[1] != ""]
+# add colun names
+test_df.columns=['Rank','City','IATA','PAX_May_2020','PAX_Vs_19','PAX_Vs_18','Commercial_May_2020', 'Commercial_Vs_19','Freight_May_2020', 'Freight_Vs_19']
+# prin Heathrows May Stats
+print(test_df[test_df['IATA'] == 'LHR'])
