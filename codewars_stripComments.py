a54c300bfca78726416a4ec437e16a1e050611ce
Adam <adambatchelor2@gmail.com>
2020-11-06 23:03:17 +0000

    Create codewars_stripComments.py

---
+++ codewars_stripComments.py
@@ -0,0 +1,15 @@
+
+# https://www.codewars.com/kata/51c8e37cee245da6b40000bd/train/python
+# Complete the solution so that it strips all text that follows any of a
+# set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
+#
+# result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+# result should == "apples, pears\ngrapes\nbananas"
+
+
+# def solution(string,markers):
+#     print(string)
+
+string = "apples, pears # and bananas\ngrapes\nbananas !apples"
+marker = ["#", "!"]
+
