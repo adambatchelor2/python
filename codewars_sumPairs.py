ebfb418e05adc8a5e7510ec7f8bdb44b1475ed0a
Adam <adambatchelor2@gmail.com>
2020-11-09 20:22:41 +0000

    Update

---
+++ codewars_sumPairs.py
@@ -0,0 +1,17 @@
+
+# https://www.codewars.com/kata/54d81488b981293527000c8f/train/python
+
+# Given a list of integers and a single sum value, return the first two values
+# (parse from the left please) in order of appearance that add up to form the sum.
+
+# sum_pairs([11, 3, 7, 5],10) - answer is 7 and 3
+
+def sum_pairs(ints,s):
+
+    for z in ints:
+
+
+
+
+
+print([11, 3, 7, 5],10)
