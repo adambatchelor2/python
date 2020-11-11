fd59dd35f9613bdecbb59ec4ebf5dee5ccf85938
Adam <36477769+adambatchelor2@users.noreply.github.com>
2020-11-11 22:20:11 +0000

    Update codewars_sumPairs.py

--- codewars_sumPairs.py
+++ codewars_sumPairs.py
@@ -6,20 +6,62 @@
 
 # sum_pairs([11, 3, 7, 5],10) - answer is 7 and 3 [7,3]
 
-def sum_pairs(ints,s):
-    myDict = {}
+# def sum_pairs(ints,s):  # Option 1 too slow
+#     myDict = {}
+#
+#     for i,z in enumerate(ints):
+#         for j,y in enumerate(ints):
+#             if z + y == s and i!=j:
+#                 if i > j:
+#                     myDict[i] = [y,z]
+#                 else:
+#                     myDict[j] = [y,z]
+#     if len(myDict) > 0:
+#         temp = min(myDict.items())
+#         return temp[1]
+#     else:
+#         return None
 
-    for i,z in enumerate(ints):
-        for j,y in enumerate(ints):
-            if z + y == s and i!=j:
-                if i > j:
-                    myDict[i] = [y,z]
-                else:
-                    myDict[j] = [y,z]
-    if len(myDict) > 0:
-        temp = min(myDict.items())
-        return temp[1]
-    else:
-        return None
 
-print(sum_pairs([10, 5, 2, 3, 7, 5],10))
+# def sum_pairs(ints, s):  # Option 2 also too slow
+#
+#     outstr = []
+#     k = 1000 # artificial high number
+#
+#     for i, z in enumerate(ints):
+#
+#         for j, y in enumerate(ints):
+#             if z + y == s and i != j:
+#                 if i < k and j < k:
+#                     if i > j:
+#                         k = i
+#                     else:
+#                         k = j
+#                     outstr = []
+#                     outstr.append(z)
+#                     outstr.append(y)
+#     if len(outstr) ==0:
+#         return None
+#     else:
+#         return outstr
+
+
+# print(sum_pairs([10, 5, 2, 3, 7, 5],10))
+
+
+from itertools import permutations
+
+inList = [10, 5, 2, 3, 7, 5]
+target = 10
+perm = permutations(inList,2)
+
+permList = list(perm)
+
+z = [x for x in permList if x[0]+x[1] == target]
+z = list(set(z)) # remove duplicates
+
+for x in z:
+    print('{} and {}'.format(x[0],x[1]))
+    print(inList.index(x[0])+inList.index(x[0]))
+
+# print(z)
