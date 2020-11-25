1210dcd3097cc26d1800eb73278331958390789c
Adam <adambatchelor2@gmail.com>
2020-11-25 17:07:57 +0000

    W

--- temp.py
+++ temp.py
@@ -1,59 +1,16 @@
 
-# https://www.codewars.com/kata/5726f813c8dcebf5ed000a6b/train/python
-# https://www.codewars.com/kata/573182c405d14db0da00064e/train/python
+# https://www.codewars.com/kata/514a024011ea4fb54200004b/train/python
 
-# A natural number is called k-prime if it has exactly k prime factors, counted with multiplicity.
-# A natural number is thus prime if and only if it is 1-prime.
+# domain_name("http://github.com/carbonfive/raygun") == "github"
+# domain_name("http://www.zombie-bites.com") == "zombie-bites"
+# domain_name("https://www.cnet.com") == "cnet"
 
-# 9 = 3 * 3 --so 2
-# 12 = 3 * 2 * 2 --so 3
+def domain_name(url):
+    import re
 
+    # x = re.search("(?:http:\\)[abz]", strIn)
+    x = re.search("(?:https://|http://|www\.)([a-zA-Z0-9\-_+%&#!]+)", url)
 
-# def is_K_primt(k,intIn):
+    return x.group(1)
 
-import math
-
-k = 3
-intIn = 12
-primeFactor = 0
-
-# get list of primes
-def getPrimes(intIn):
-    maxVal = int(intIn/2) # limit test upper range
-    prime = []
-    for x in range(2,maxVal):
-        count = 0
-        for y in range(2,x):
-            if x % y == 0:
-                count += 1
-        if count == 0:
-            prime.append(x)
-    return prime
-
-def getFactors(intIn):
-    maxVal = int(intIn/2) # limit test upper range
-    factor = []
-    for x in range(1,maxVal+1):
-        if intIn % x == 0:
-            factor.append(x)
-    return factor
-
-# divide int by all less than value
-print(getPrimes(intIn))
-print(getFactors(intIn))
-
-factors = getFactors(intIn)
-primes = getPrimes(intIn)
-
-for i,x in enumerate(factors):
-    for y in primes:
-        if x % y == 0 and x / y > 1:
-            factors[i] = int(x / y)
-            factors.append(int(x - (x / y)))
-
-outList = [x for x in factors if x in primes]
-
-print(factors)
-print(factors[::-1])
-print(outList)
-# print('The prime factor of {} is {}'.format(intIn,primeFactor))
+print (domain_name("www.icann.org"))
